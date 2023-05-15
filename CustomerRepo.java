8e4a4064b90a3b0872878b01a7ac95781940bb06
Shihab Ahmed <57572719+ahmedshihab809@users.noreply.github.com>
2023-05-15 22:19:44 +0600

    num1

---
+++ CustomerRepo.java
@@ -0,0 +1,13 @@
+package Web-Arch-Project;
+
+package com.Project.SpringAngular.CustomerRepo;
+
+import com.Project.SpringAngular.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+}
